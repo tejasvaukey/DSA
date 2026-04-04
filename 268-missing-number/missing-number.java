7963e660873bcd6a6414e204754f2a201fd81c7b
Tejasva Ukey <115536499+tejasvaukey@users.noreply.github.com>
2026-04-04 12:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (75.23%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        int reqSum = n*(n+1)/2;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+        }
+        int missing = reqSum - sum;
+        return missing;
+    }
+}
