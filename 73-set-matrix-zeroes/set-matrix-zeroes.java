807de1bdcfd539df60dc09243d7b2aa5af04ec5c
Tejasva Ukey <115536499+tejasvaukey@users.noreply.github.com>
2026-04-30 22:14:33 +0530

    Time: 1 ms (94.30%) | Memory: 47.7 MB (39.93%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,18 +1,29 @@
 class Solution {
     public void setZeroes(int[][] m) {
-        boolean[] col = new boolean[m[0].length];
-        boolean[] row = new boolean[m.length];
+        boolean fr = false, fc = false;
         for(int i=0; i<m.length; i++){
             for(int j=0; j<m[0].length; j++){
                 if(m[i][j] == 0){
-                    row[i] = true;
-                    col[j] = true;
+                    if(i == 0) fr = true;
+                    if(j == 0) fc = true;
+                    m[0][j] = 0;
+                    m[i][0] = 0;
                 }
             }
         }
-        for(int i=0; i<m.length; i++){
+        for(int i=1; i<m.length; i++){
+            for(int j=1; j<m[0].length; j++){
+                if(m[i][0] == 0 || m[0][j] == 0) m[i][j] = 0;
+            }
+        }
+        if(fr){
             for(int j=0; j<m[0].length; j++){
-                if(row[i] == true || col[j] == true) m[i][j] = 0;
+                m[0][j] = 0;
+            }
+        }
+        if(fc){
+            for(int i=0; i<m.length; i++){
+                m[i][0] = 0;
             }
         }
         
